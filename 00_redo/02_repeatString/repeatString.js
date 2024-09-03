d01d514514623144c411d877db1e2828f5e6f43d
Kian <Kian.berg333@gmail.com>
2024-09-03 17:49:50 -0600

    Complete 02 by concatenating string in a for loop

--- 00_redo/02_repeatString/repeatString.js
+++ 00_redo/02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, repeats) {
+    let repeatedString = "";
+    if(repeats < 0) {
+        return "ERROR";
+    }
+    for(let loopCounter = repeats; loopCounter > 0; loopCounter --) {
+        repeatedString += string;
+    }
+    return repeatedString;
 };
 
 // Do not edit below this line
