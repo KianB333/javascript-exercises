b9c479fdf5fd79a8392f0efc2e22a2c151ba6e84
Kian <Kian.berg333@gmail.com>
2024-09-04 18:09:48 -0600

    Complete 03 by copying letters from end to reverse

--- 00_redo/03_reverseString/reverseString.js
+++ 00_redo/03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+    let newWord = "";
+    for(let loopCounter = 1; loopCounter <= word.length; loopCounter++) {
+        newWord += word.substr(-loopCounter, 1);
+    }
+    return newWord;
 };
 
 // Do not edit below this line
