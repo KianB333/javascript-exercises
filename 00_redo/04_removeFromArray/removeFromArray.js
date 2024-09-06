c836eaf8e3e6a753b00a89d101c35894de17fef5
Kian <Kian.berg333@gmail.com>
2024-09-06 12:47:19 -0600

    Complete 04
    Runs nested loop to remove unwanted items from a given array.

--- 00_redo/04_removeFromArray/removeFromArray.js
+++ 00_redo/04_removeFromArray/removeFromArray.js
@@ -1,4 +1,17 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...badElem) {
+    let newArray = [];
+    for(let loopCounter = 0; loopCounter < array.length; loopCounter++) {
+        let forbidden = false;
+        badElem.forEach(function(elem) {
+            if(array[loopCounter] === elem) {
+                forbidden = true;
+            }
+        });
+        if(forbidden == false) {
+            newArray.push(array[loopCounter]);
+        };
+    };
+    return newArray;
 };
 
 // Do not edit below this line
