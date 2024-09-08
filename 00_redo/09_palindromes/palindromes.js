28db10398d675d37a2375196728a7d12aa9f6c56
Kian <Kian.berg333@gmail.com>
2024-09-07 22:53:54 -0600

    Complete 09

--- 00_redo/09_palindromes/palindromes.js
+++ 00_redo/09_palindromes/palindromes.js
@@ -1,5 +1,17 @@
-const palindromes = function () {
-
+const palindromes = function (word) {
+    let forward = word.replace(/[!"#$%&'()*+,-./:;<=>?@[\]^_`{|}~ ]/g, '').toLowerCase();
+    //Removes punctuation, whitespaces, and capitalization
+    let backward = "";
+    for(let loopCounter = -1; loopCounter >= -forward.length; loopCounter--) {
+        backward += forward.substr(loopCounter, 1);
+    }
+    //reverses word
+    if (forward == backward) {
+        return true;
+    } else {
+        return false;
+    };
+    //compares forward and backward
 };
 
 // Do not edit below this line
