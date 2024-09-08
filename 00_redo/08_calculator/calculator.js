40541d64d40576c7fba82ceff21ae117c52b1472
Kian <Kian.berg333@gmail.com>
2024-09-07 22:18:45 -0600

    Complete 08

--- 00_redo/08_calculator/calculator.js
+++ 00_redo/08_calculator/calculator.js
@@ -1,25 +1,28 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(nums) {
+  return nums.reduce((total, num) => total += num, 0);
+};
+const multiply = function(nums) {
+  return nums.reduce((total, num) => total *= num, 1);
 };
 
-const multiply = function() {
-
+const power = function(a, b) {
+  return a ** b;
 };
 
-const power = function() {
-	
-};
-
-const factorial = function() {
-	
+const factorial = function(num) {
+	let factorial = 1;
+  for(let loopCounter = 1; loopCounter <= num; loopCounter++) {
+    factorial *= loopCounter;
+  };
+  return factorial;
 };
 
 // Do not edit below this line
