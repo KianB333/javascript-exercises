209768daf05652ac88bcfcd013227d5a6a580956
Kian <Kian.berg333@gmail.com>
2024-09-07 23:25:39 -0600

    Complete 10

--- 00_redo/10_fibonacci/fibonacci.js
+++ 00_redo/10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(pos) {
+    if(pos < 0) {
+        return "OOPS";
+    };
+    //checks for negatives
+    let result = 0;
+    let resultStorage;
+    let prevResult = 1;
+    //prevResult being 1 and result being 0 gives a starting point for sequence addition. Position 0 is "0".
+    for(let loopCounter = 0; loopCounter < pos; loopCounter++) {
+        resultStorage = result;
+        result += prevResult;
+        prevResult = resultStorage;
+    };
+    //Fibonacci calculations
+    return result;
 };
 
 // Do not edit below this line
