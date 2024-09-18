71a64f373fb76651779023d035d50ebbaf1a53e7
Kian <Kian.berg333@gmail.com>
2024-09-17 19:22:30 -0600

    Complete 12
    .map() and .sort() cannot chain. Must be separate for some reason.

--- 00_redo/12_findTheOldest/findTheOldest.js
+++ 00_redo/12_findTheOldest/findTheOldest.js
@@ -1,5 +1,8 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(people) {
+    people.map(person => person.age = person.yearOfDeath ? person.yearOfDeath - person.yearOfBirth : (new Date()).getFullYear() - person.yearOfBirth);
+    //Creates person.age, even if no yearOfDeath exists
+    people.sort((a, b) => a.age > b.age ? -1 : 1);
+    return people[0];
 };
 
 // Do not edit below this line
